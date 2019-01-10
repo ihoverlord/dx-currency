7037c1a205cd936cd847246173775fde67c876c7
biryanibabu <ghimakarreddy@gmail.com>
2019-01-10 22:35:30 +0530

    scrapper done,data filtering done, api created,response sentt

---
+++ app.js
@@ -0,0 +1,15 @@
+const scrapper = require('./lib/scrapper')
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(helmet())
+app.get('/', scrapper.data)
+
+app.listen(port, () => console.log(`app listening on port ${port}!`))
+
+
